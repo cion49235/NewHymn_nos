6025c9bb98f08c8dcf4439aba040ed98c4d4fe1f
cion49235 <cion49235@nate.com>
2017-12-17 19:25:28 +0900

    [20171217]

---
+++ src/song/newhymn/view/nos/HymnApp.java
@@ -0,0 +1,39 @@
+package song.newhymn.view.nos;
+
+
+import android.app.Application;
+
+import song.newhymn.view.nos.util.NetworkHelper;
+
+
+public class HymnApp extends Application{
+	private static HymnApp INSTANCE			= null;
+	/* (non-Javadoc)
+	 * @see android.app.Application#onCreate()
+	 */
+	@Override
+	public void onCreate() {
+		INSTANCE = this;
+	}
+
+	public static HymnApp getApplication(){
+		return INSTANCE;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see android.app.Application#onTerminate()
+	 */
+	@Override
+	public void onTerminate() {
+
+		NetworkHelper.getInstance().close();
+
+		INSTANCE = null;
+		super.onTerminate();
+	}
+	
+	public static HymnApp getInstance() {
+		return INSTANCE;
+  }
+}
